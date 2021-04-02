547009f9f56ebdb88833c908f2e3b4089bdce0ec
kerbalremedies <kerbalremedies@hotmail.com>
2021-04-02 21:45:55 +0100

    First few python programs!

---
+++ ok.py
@@ -0,0 +1,14 @@
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries -1
+        if retries < 0:
+            raise ValueError('Invalid user respone')
+        print(reminder)
+
+
+ask_ok('Do you really want to wipe your hard drive and throw it into the river?')
