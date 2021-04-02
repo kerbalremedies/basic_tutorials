168af1d8c4c46d74675cb26c4872a527c831577c
kerbalremedies <kerbalremedies@hotmail.com>
2021-04-02 21:47:19 +0100

    cosmetic tweak to ok.py

--- ok.py
+++ ok.py
@@ -11,4 +11,4 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         print(reminder)
 
 
-ask_ok('Do you really want to wipe your hard drive and throw it into the river?')
+ask_ok('Do you really want to wipe your hard drive and throw it into the river? ')
